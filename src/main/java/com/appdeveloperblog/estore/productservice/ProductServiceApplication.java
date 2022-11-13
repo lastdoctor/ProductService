232e216a432a41e1bec4474e2cbfda437b2a66d8
Kubatov Susa <melastdoctorwho@gmail.com>
2022-11-13 17:37:12 +0600

    init

--- src/main/java/com/appdeveloperblog/estore/productservice/ProductServiceApplication.java
+++ src/main/java/com/appdeveloperblog/estore/productservice/ProductServiceApplication.java
@@ -8,7 +8,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 public class ProductServiceApplication {
     public static void main(String[] args) {
-        System.out.println("hello");
         SpringApplication.run(ProductServiceApplication.class, args);
     }
 }
